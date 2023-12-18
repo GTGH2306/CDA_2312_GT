e164b948dc146baf73ac6cae5b084dedd825e3b0
GRANDGIRARD Tristan <TGrandgirard@arfp.asso.fr>
2023-12-18 16:55:02 +0100

    commit 18/12

---
+++ Algo_Csharp/CalculAireVolumeSphere/CalculAireVolumeSphere/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CalculAireVolumeSphere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Saisissez le rayon de la sphère: ");
+            float rayon = float.Parse(Console.ReadLine());
+            float aire = (float)(4 * (Math.PI) * Math.Pow(rayon, 2));
+            Console.WriteLine("L'aire de la sphère est de " + aire);
+            float volume = (float)((4d / 3d) * Math.PI * Math.Pow(rayon, 3));
+            Console.WriteLine("Le volume de la sphère est de " + volume);
+        }
+    }
+}
