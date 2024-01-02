b9fc47deea419d221426be94b8907b5afdc62eb8
GRANDGIRARD Tristan <TGrandgirard@arfp.asso.fr>
2024-01-02 16:52:39 +0100

    02/01/24

---
+++ Algo_Csharp/FruitsEtLegumes/FruitsEtLegumes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FruitsEtLegumes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string saisie = "";
+            List<string> legumes = new List<string>();
+            List<double> prix = new List<double>();
+            int indexMoinsChere = -1;
+            while (saisie != "go")
+            {
+                Console.WriteLine("Saisissez un fruit/légume (go pour arrêter):");
+                saisie = Console.ReadLine();
+                if (saisie != "go")
+                {
+                    legumes.Add(saisie);
+                    Console.WriteLine("Saisissez le prix au kg:");
+                    prix.Add(double.Parse(Console.ReadLine()));
+                    if (verifMoinsCher(prix))
+                    {
+                        indexMoinsChere = prix.Count - 1;
+                    }
+                }
+            }
+            for (int i = 0; i < legumes.Count; i++)
+            {
+                Console.WriteLine("1 kilogramme de " + legumes[i] + " coute " + prix[i] + " euros.");
+            }
+            Console.WriteLine("Légume/fruit le moins cher au kilo: " + legumes[indexMoinsChere]);
+
+        }
+
+        static bool verifMoinsCher(List<double> _listePrix)
+        {
+            bool retour = true;
+            if (_listePrix.Count > 1)
+            {
+                for (int i = 0; i < _listePrix.Count - 1; i++)
+                {
+                    if (_listePrix[_listePrix.Count - 1] > _listePrix[i])
+                    {
+                        retour = false;
+                    }
+                }
+            }
+            return retour;
+        }
+    }
+}
---
+++ Algo_Csharp/JeuZeroDeux/JeuZeroDeux/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Security.Cryptography;
+
+namespace JeuZeroDeux
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valPlayer, valOrdi;
+            int scorePlayer = 0;
+            int scoreOrdi = 0;
+            Random rnd = new Random();
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Saisissez un nombre entre 0 et 2 (nombre négatif pour quitter):");
+                    valPlayer = int.Parse(Console.ReadLine());
+                } while (valPlayer > 2);
+                if (valPlayer >= 0)
+                {
+                    valOrdi = rnd.Next(0, 3);
+                    Console.WriteLine("L'ordinateur a choisi " + valOrdi);
+                    switch (valPlayer - valOrdi)
+                    {
+                        case 0:
+                            Console.WriteLine("Egalité");
+                            break;
+                        case -1:
+                            Console.WriteLine("Joueur gagne 1pts");
+                            scorePlayer++;
+                            break;
+                        case -2:
+                            Console.WriteLine("Ordinateur gagne 1pts");
+                            scoreOrdi ++;
+                            break;
+                        case 1:
+                            Console.WriteLine("Ordinateur gagne 1pts");
+                            scoreOrdi++;
+                            break;
+                        case 2:
+                            Console.WriteLine("Joueur gagne 1pts");
+                            scorePlayer ++;
+                            break;
+                    }
+                    Console.WriteLine("Joueur: " + scorePlayer);
+                    Console.WriteLine("Ordinateur: " + scoreOrdi)
+                        ;
+                }
+            } while (valPlayer >= 0 && scorePlayer < 10 && scoreOrdi < 10);
+
+            if (scorePlayer > scoreOrdi)
+            {
+                Console.WriteLine("Le joueur gagne " + scorePlayer + " à " + scoreOrdi);
+            } else if (scorePlayer < scoreOrdi)
+            {
+                Console.WriteLine("L'ordinateur gagne " + scoreOrdi + " à " + scorePlayer);
+            } else
+            {
+                Console.WriteLine("Egalité " + scorePlayer + " à " + scoreOrdi);
+            }
+        }
+    }
+
+}
