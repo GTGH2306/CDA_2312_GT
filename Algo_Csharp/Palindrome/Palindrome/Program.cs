6d8be87c3e6d89c00fd84d0a47e813a29ec57377
GRANDGIRARD Tristan <TGrandgirard@arfp.asso.fr>
2024-01-03 16:29:52 +0100

    03/01

---
+++ Algo_Csharp/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string saisie;
+            do
+            {
+                Console.WriteLine("Saisissez une entrée: ");
+                saisie = Console.ReadLine();
+
+                if (IsPalindrome(Unform(saisie)) == result.Vrai)
+                {
+                    Console.WriteLine(saisie + " est un palindrome.");
+                }
+                else if (IsPalindrome(Unform(saisie)) == result.Faux)
+                {
+                    Console.WriteLine(saisie + " n'est pas un palindrome.");
+                } else if (IsPalindrome(Unform(saisie)) == result.Vide)
+                {
+                    Console.WriteLine("CHAÎNE VIDE, MERCI D'APPRENDRE A ECRIRE");
+                } else if (IsPalindrome(Unform(saisie)) == result.Insuf)
+                {
+                    Console.WriteLine("BESOIN DE PLUS D'UN CARACTERE");
+                } else
+                {
+                    Console.WriteLine("ERROR, OUT OF BOUNDS");
+                }
+
+            } while (saisie != "q");
+        }
+
+        static string Unform(string _phrase)
+        {
+            StringBuilder retour = new StringBuilder();
+
+            _phrase = _phrase.ToLower();
+            for (int i = 0; i < _phrase.Length; i++)
+            {
+                if (_phrase[i] != ' ')
+                {
+                    retour.Append(_phrase[i]);
+                }
+            }
+            return retour.ToString();
+        }
+
+        static result IsPalindrome(string _phrase)
+        {
+            result retour;
+            if (_phrase.Length > 1)
+            {
+                StringBuilder reverse = new StringBuilder();
+                for (int i = 0; i < _phrase.Length; i++)
+                {
+                    reverse.Append(_phrase[(_phrase.Length - 1) - i]);
+                }
+
+                if (reverse.ToString() == _phrase)
+                {
+                    retour = result.Vrai;
+                }
+                else
+                {
+                    retour = result.Faux;
+                }
+            } else if (_phrase.Length == 1)
+            {
+                retour = result.Insuf;
+            } else
+            {
+                retour = result.Vide;
+            }
+            return retour;
+        }
+
+        enum result
+        {
+            Vrai,
+            Faux,
+            Vide,
+            Insuf
+        }
+    }
+}
---
+++ Algo_Csharp/nbJeunes/nbJeunes/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace nbJeunes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list1 = new int[] {45, 35, 65, 76, 34, 32, 31, 46, 57, 68, 75, 46, 53, 36, 31, 46, 68, 59, 30, 20};
+            int[] list2 = new int[] {15, 5, 5, 6, 4, 2, 11, 16, 7, 8, 7, 3, 13, 16, 11, 18, 8, 9, 19, 3};
+            int[] list3 = new int[] {45, 35, 65, 76, 34, 20, 20, 30, 30, 30, 20, 20, 30, 20, 30, 20, 20, 8, 15, 23};
+
+            Console.WriteLine(nbJeunesSaisie(20));
+            Console.WriteLine(nbJeunes(list1));
+            Console.WriteLine(nbJeunes(list2));
+            Console.WriteLine(nbJeunes(list3));
+        }
+
+        static string nbJeunesSaisie(int _size)
+        {
+            int[] listSaisie = new int[_size];
+
+            for (int i = 0; i < listSaisie.Length; i++)
+            {
+                Console.WriteLine("Saisissez l'âge de la personne n°" + (i + 1) + ":");
+                listSaisie[i] = int.Parse(Console.ReadLine());
+            }
+            return nbJeunes(listSaisie);
+        }
+        static string nbJeunes(int[] _list)
+        {
+            int jeunes = 0;
+            int vieux = 0;
+            int vingt = 0;
+            for (int i = 0; i < _list.Length; i++)
+            {
+                if (_list[i] < 20)
+                {
+                    jeunes++;
+                } else if (_list[i] > 20)
+                {
+                    vieux++;
+                } else
+                {
+                    vingt++;
+                }
+            }
+            if (jeunes == _list.Count())
+            {
+                return ("TOUTES LES PERSONNES ONT MOINS DE 20 ANS");
+            } else if (vieux == _list.Count())
+            {
+                return ("TOUTES LES PERSONNES ONT PLUS DE 20 ANS");
+            } else
+            {
+                return (jeunes + " - de 20, " + vieux + " + de 20," + vingt + " = à 20");
+            }
+        }
+    }
+}
