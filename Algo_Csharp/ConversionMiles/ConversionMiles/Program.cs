afc1b835539023856e2057d74c09dcee43663b09
GRANDGIRARD Tristan <TGrandgirard@arfp.asso.fr>
2024-01-04 11:20:39 +0100

    Update Convertisseur Km/Mi

--- Algo_Csharp/ConversionMiles/ConversionMiles/Program.cs
+++ Algo_Csharp/ConversionMiles/ConversionMiles/Program.cs
@@ -1,68 +1,91 @@
-﻿namespace ConversionMiles
+﻿using System.Text;
+
+namespace ConversionMiles
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string saisie;
+            RetourSaisie retourSaisie;
+            const double rapportKmMi = 1.609;
+            double km, mi;
+
             do
             {
                 do
                 {
                     Console.WriteLine("Saisissez une distance à convertir:\t\t(Q pour Quitter)");
                     saisie = Console.ReadLine();
-                    saisie = saisie.ToLower();
+                    retourSaisie = CtrlSaisie(saisie);
 
-                    if (CtrlSaisie(saisie) == RetourSaisie.Invalide)
+                    if (retourSaisie == RetourSaisie.Invalide)
                     {
                         Console.WriteLine("Saisie invalide.");
                     }
-                } while (CtrlSaisie(saisie) == RetourSaisie.Invalide);
+                } while (retourSaisie == RetourSaisie.Invalide); //Demande une valeur tant qu'elle n'est pas valide
 
 
 
-                if (CtrlSaisie(saisie) != RetourSaisie.Quit)
+                if (retourSaisie != RetourSaisie.Quit) //N'execute pas le programme si l'utilateur à quitté
                 {
-
-                    if (CtrlSaisie(saisie) == RetourSaisie.FromKM)
+                    switch (retourSaisie)
                     {
-                        double km = double.Parse(saisie.Substring(0, saisie.IndexOf("km")));
-                        double mi = km / 1.609;
-
-                        Console.WriteLine(Math.Round(km * 100) / 100 + "km = " + Math.Round(mi * 100) / 100 + "mi");
-                    } else if (CtrlSaisie(saisie) == RetourSaisie.FromMI)
-                    {
-                        double mi = double.Parse(saisie.Substring(0, saisie.IndexOf("mi")));
-                        double km = mi * 1.609;
-
-                        Console.WriteLine(Math.Round(km * 100) / 100 + "km = " + Math.Round(mi * 100) / 100 + "mi");
-                    } else
-                    {
-                        double km = double.Parse(saisie);
-                        double mi = km / 1.609;
-
-                        Console.WriteLine(Math.Round(km * 100) / 100 + "km = " + Math.Round(mi * 100) / 100 + "mi");
+                        case RetourSaisie.FromKM:
+                            km = ToDouble(saisie);
+                            mi = km / rapportKmMi;
+                            break;
+                        case RetourSaisie.FromMI:
+                            mi = ToDouble(saisie);
+                            km = mi * rapportKmMi;
+                            break;
+                        default:
+                            km = double.NaN;
+                            mi = double.NaN;
+                            Console.WriteLine("Erreur: Unité non programmée");
+                            break;
                     }
+
+                    Console.WriteLine((Math.Round(km * 100) / 100) + "km = " + (Math.Round(mi * 100) / 100) + "mi.");
                 }
 
-            }while (CtrlSaisie(saisie) != RetourSaisie.Quit);
+            }while (retourSaisie != RetourSaisie.Quit);
         }
 
-        static RetourSaisie CtrlSaisie(string _saisie)
+        static double ToDouble(string _valeur) //Converti un string en double en retirant les caractères
+        {
+            int virgule = 0;
+            StringBuilder noTextString = new();
+
+            for (int i = 0; i < _valeur.Length; i++)
+            {
+                if (_valeur[i] == '-' && i == 0)
+                {
+                    noTextString.Append(_valeur[i]);
+                } else if (_valeur[i] == ',' && virgule == 0)
+                {
+                    noTextString.Append(_valeur[i]);
+                    virgule++;
+                } else if (Char.IsNumber(_valeur[i]))
+                {
+                    noTextString.Append(_valeur[i]);
+                }
+            }
+            return double.Parse(noTextString.ToString());
+        }
+
+        static RetourSaisie CtrlSaisie(string _saisie) //Renvoi une enum retour selon la saisie en paramêtre
         {
             RetourSaisie retour;
+            _saisie = _saisie.ToLower();
 
-            if(_saisie == "q" || _saisie == "je veux quitter svp, merci.")
+            if (_saisie == "q" || _saisie == "je veux quitter svp, merci.")
             {
                 retour = RetourSaisie.Quit;
-            } else if (double.TryParse(_saisie, out _))
-            {
-                retour = RetourSaisie.Defaut;
-            } else if (_saisie.EndsWith("km") && (double.TryParse(_saisie.Substring(0, _saisie.IndexOf("km")), out _)))
+            } else if (double.TryParse(_saisie, out _) || (_saisie.EndsWith("km") && (double.TryParse(_saisie.Substring(0, _saisie.IndexOf("km")), out _))))
             {
                 retour = RetourSaisie.FromKM;
-            }
-            else if (_saisie.EndsWith("mi") && (double.TryParse(_saisie.Substring(0, _saisie.IndexOf("mi")), out _)))
+            } else if (_saisie.EndsWith("mi") && (double.TryParse(_saisie.Substring(0, _saisie.IndexOf("mi")), out _)))
             {
                 retour = RetourSaisie.FromMI;
             } else
@@ -74,7 +97,6 @@
 
         enum RetourSaisie
         {
-            Defaut,
             FromKM,
             FromMI,
             Quit,
