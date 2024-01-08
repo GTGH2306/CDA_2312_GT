ed468e0819bf61e0a2c6e557e405504263310e5e
GRANDGIRARD Tristan <TGrandgirard@arfp.asso.fr>
2024-01-08 16:51:04 +0100

    08/01

---
+++ Algo_Csharp/PassageParRef/PassageParRef/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PassageParRef
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 17;
+            Console.WriteLine(age);
+            AjouterUnAn(ref age);
+            Console.WriteLine(age);
+        }
+
+        static void AjouterUnAn(ref int _age)
+        {
+            _age++;
+        }
+    }
+}
