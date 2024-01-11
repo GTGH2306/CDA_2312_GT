01e3341b27adf102746d86c25a966b52bb8e39d3
GRANDGIRARD Tristan <TGrandgirard@arfp.asso.fr>
2024-01-11 16:49:33 +0100

    11/01

---
+++ Algo_Csharp/EvaluationExpress/Correction/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Correction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valeurs = { 2, 4, 1, 8, 6, 14, 23, 25, 7, 42 };
+            double moyenne = 13.2;
+            
+        }
+    }
+}
