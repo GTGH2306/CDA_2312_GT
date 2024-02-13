9bf97ae033d2680e34e2093d12291d3d798d7531
TGrandgirard PC-Cours <137295165+GTGH2306@users.noreply.github.com>
2024-02-13 16:55:46 +0100

    13/02

--- web/www/babane/scripts/script.js
+++ web/www/babane/scripts/script.js
@@ -1,20 +1,29 @@
+let farmsProps = [Babanier, Contrat, Hydro, Nft, Robot]
+let farmTab = [];
 let babanes = 0
-const tickrate = 200;
+const tickrate = 100;
 let timeoutHandle;
 
 
 window.addEventListener('load', function() {
     
+    for (let i = 0; i < farmsProps.length; i++){
+        farmTab.push(new Farm(farmsProps[i]));
+        elementForFarm(farmTab[i]);
+    }
     
     setInterval(function(){
-        showBabanes();
+        babanes += (getGlobalBps() / 1000) * tickrate;
         
-    });
+        showBabanes();
+    }, tickrate);
 });
+
+
 
 document.getElementById('babaneImg').addEventListener('click', function(){
     playAnimation();
-    babanes ++;
+    babanes += 1 + farmTab[0].getBps();
     showBabanes();
 });
 
@@ -39,5 +48,92 @@ function playAnimation(){
 }
 
 function showBabanes(){
-    document.getElementById('babaneNb').innerHTML = babanes;
+    document.getElementById('babaneNb').innerHTML = Math.round(babanes*10)/10;
+}
+
+function Farm(_proprietes){
+    this.id = _proprietes.id;
+    this.nom = _proprietes.nom;
+    this.initCost = _proprietes.initCost;
+    this.initBps = _proprietes.initBps;
+    this.img = _proprietes.img;
+    this.numberOf = 0;
+    this.getBps = function(){
+        return Math.round(this.numberOf * this.initBps * 10)/10;
+    }
+    this.getCost = function(){
+        return Math.floor(Math.pow(this.initCost, (1 + (this.numberOf / 11))));
+    };
+    this.buy = function(){
+        if (babanes - this.getCost() >= 0){
+            babanes -= this.getCost();
+            this.numberOf ++;
+            refreshFarmElements();
+        }
+    }
+}
+
+function elementForFarm(_farm){
+    let element = document.createElement('div');
+    let titre = document.createElement('h3');
+    let icon = document.createElement('img');
+    let pFarmNb = document.createElement('p');
+    let buyButton = document.createElement('input');
+    let pPrixTxt = document.createElement('p');
+    let pPrixNb = document.createElement('p');
+    let pBpsTxt = document.createElement('p');
+    let pBpsNb = document.createElement('p');
+
+    element.classList.add('farm');
+
+    titre.innerHTML= _farm.nom;
+    element.appendChild(titre);
+
+    icon.src = _farm.img;
+    element.appendChild(icon);
+
+    pFarmNb.id = _farm.id + 'Nb';
+    pFarmNb.classList.add('numberOf');
+    pFarmNb.innerHTML = 'x ' + _farm.numberOf;
+    element.appendChild(pFarmNb);
+
+    buyButton.type = 'button';
+    buyButton.value = 'Acheter';
+    buyButton.id = _farm.id + 'Buy';
+    buyButton.addEventListener('click', () => _farm.buy());
+    element.appendChild(buyButton);
+
+    pPrixTxt.innerHTML = 'Prix: ';
+    element.appendChild(pPrixTxt);
+
+    pPrixNb.id = _farm.id + 'Prix';
+    pPrixNb.classList.add('tag');
+    pPrixNb.innerHTML = _farm.getCost();
+    element.appendChild(pPrixNb);
+
+    pBpsTxt.innerHTML = 'BPS: ';
+    element.appendChild(pBpsTxt);
+
+    pBpsNb.id = _farm.id + 'Bps';
+    pBpsNb.classList.add('tag');
+    pBpsNb.innerHTML = _farm.getBps();
+    element.appendChild(pBpsNb);
+
+    document.getElementById('farms').appendChild(element);
+}
+
+function refreshFarmElements(){
+    for (let farm of farmTab){
+        document.getElementById(farm.id + 'Nb').innerHTML = 'x ' + farm.numberOf;
+        document.getElementById(farm.id + 'Prix').innerHTML = farm.getCost();
+        document.getElementById(farm.id + 'Bps').innerHTML = farm.getBps();
+    }
+}
+
+function getGlobalBps(){
+    let retour = 0;
+    for (let farm of farmTab){
+        retour += farm.getBps();
+    }
+    return retour;
 }
